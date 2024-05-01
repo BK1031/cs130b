9a077cc3ef4342b2f52069e47a0c3383fa79f21e
Bharat Kathi <bharat1031@gmail.com>
2024-04-30 23:37:09 -0700

    trying checkerboard again

--- pa1/checkerboard.py
+++ pa1/checkerboard.py
@@ -32,44 +32,39 @@ Output a single integer representing the minimum sum possible by replacing the 0
 valid Magic Checkerboard. Output -1 if it is not possible to replace the 0 cells to meet the constraints of a Magic Checkerboard.
 """
 
-def is_valid(board, i, j, value):
-    if any(board[i][col] == value for col in range(len(board[0]))):
-        return False
-
-    if any(board[row][j] == value for row in range(len(board))):
-        return False
-
-    neighbors = [(i-1, j-1), (i-1, j+1), (i+1, j-1), (i+1, j+1)]
-    for row, col in neighbors:
-        if 0 <= row < len(board) and 0 <= col < len(board[0]):
-            if board[row][col] != 0 and board[row][col] % 2 == value % 2:
+def is_valid(n, m, board):
+    for row in range(n):
+        for col in range(1, m):
+            if board[row][col] <= board[row][col - 1]:
+                return False
+    for row in range(n - 1):
+        for col in range(m - 1):
+            if (board[row][col] % 2 == board[row + 1][col] % 2) and (board[row][col] != 0 and board[row + 1][col] != 0):
+                return False
+            if (board[row][col] % 2 == board[row][col + 1] % 2) and (board[row][col] != 0 and board[row][col + 1] != 0):
                 return False
     return True
 
-def recurse(board, i, j):
-    if i == len(board):
-        return True
-    if j == len(board[0]):
-        return recurse(board, i + 1, 0)
-    if board[i][j] != 0:
-        return recurse(board, i, j + 1)
-    for value in range(1, 2001):
-        if is_valid(board, i, j, value):
-            board[i][j] = value
-            if recurse(board, i, j + 1):
-                return True
-            board[i][j] = 0
-    return False
-
-def solve_magic_checkerboard(n, m, board):
-    if n % 2 != 0 and m % 2 != 0:
-        return -1
+def solve(n, m, board):
     if n % 2 != m % 2:
         return -1
-    if not recurse(board, 0, 0):
-        return -1
+
+    for row in range(n):
+        prev_value = 0
+        for col in range(m):
+            if board[row][col] == 0:
+                for value in range(prev_value + 1, prev_value + 2 * n * m + 1):
+                    board[row][col] = value
+                    if is_valid(n, m, board):
+                        break
+                    board[row][col] = 0
+                else:
+                    return -1
+            prev_value = board[row][col]
+
     return sum(sum(row) for row in board)
 
 n, m = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(n)]
-print(solve_magic_checkerboard(n, m, board))
+
+print(solve(n, m, board))
